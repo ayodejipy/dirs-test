655942be4ac17369f125097d10e2146d077c6bda
ayodejipy <ay.programmr@gmail.com>
2023-06-15 18:53:07 +0100

    Revert "fix: adding new menu overriding edited menu"
    This reverts commit 2fb174253cf5ec7904f06777e361cabc67eaa611.

--- app/src/store/modal.ts
+++ app/src/store/modal.ts
@@ -1,74 +1,74 @@
-import { defineStore } from 'pinia'
-import { ref, reactive } from 'vue'
+import { defineStore } from "pinia";
+import { ref, reactive } from "vue";
 import useApi from "../composables/api";
 
-import { IMealDetails, MetaData } from '../utils/types';
+import { IMealDetails } from "../utils/types";
 
+export const useModalStore = defineStore("modal", () => {
+    const { getMenus } = useApi();
+    const isOpen = ref<boolean>(false);
+    const action = ref<string>("add");
 
+    const menus = ref<IMealDetails[]>([]);
+    const request = reactive({
+        loading: false,
+        isSuccess: false,
+        isError: false,
+        error: null,
+    });
 
-export const useModalStore = defineStore('modal', () => {
-	const { getMenus } = useApi();
-	const isOpen = ref<boolean>(false)
-	const action = ref<string>('add')
+    const form = ref<Partial<IMealDetails>>({
+        // _id: "",
+        // _Changed: "",
+        // _Created: "",
+        name: "",
+        imageUrl: "",
+        description: "",
+        mealCategory: "",
+        mealTime: "",
+        price: "",
+        waitingTime: "",
+        isAvailable: true,
+    });
 
-	const menus = ref<IMealDetails[]>([]);
-	const request = reactive({
-		loading: false,
-		isSuccess: false,
-		isError: false,
-		error: null,
-	});
+    function toggleModal() {
+        isOpen.value = !isOpen.value;
+    }
 
-	const form = reactive({
-		name: "",
-		imageUrl: "",
-		description: "",
-		mealCategory: "",
-		mealTime: "",
-		price: "",
-		waitingTime: "",
-		isAvailable: true,
-	});
+    function updateForm(body: IMealDetails) {
+        form.value = { ...body };
+    }
 
-	function toggleModal() {
-    	isOpen.value = !isOpen.value;
-	}
+    async function fetchMenus(): Promise<void> {
+        try {
+            request.loading = true;
+            const {
+                data: { data: data },
+            } = await getMenus();
+            menus.value = data;
+            request.error = null;
+        } catch (error: any) {
+            request.isError = true;
+            request.error = error.message;
+        } finally {
+            request.loading = false;
+        }
+    }
 
-	function updateForm(body: IMealDetails) {
-		form.name = body.name,
-		form.imageUrl = body.imageUrl,
-		form.description = body.description,
-		form.mealCategory =  body.mealCategory
-		form.mealTime = body.mealTime
-		form.price = body.price
-		form.waitingTime = body.waitingTime
-		form.isAvailable = body.isAvailable
-		form._id = body._id
-		form._Changed = body._Changed
-		form._Created = body._Created
-	}
+    function clearForm() {
+        // get object keys
+        const formKeys = Object.keys(form.value);
 
-	async function fetchMenus(): Promise<void> {
-		try {
-			request.loading = true
-			const { data: { data: data } } = await getMenus();
-			menus.value = data;
-			request.error = null
-		} catch (error: any) {
-			request.isError = true
-			request.error = error.message
-		} finally {
-			request.loading = false;
-		}
-			
-	}
+        // loop through form keys and set values as empty string
+        for (const key of formKeys) {
+            console.log(key);
+            if (key == "isAvailable") form.value[key] = false;
+            form.value[key] = "";
+        }
+        // removed keys not needed
+        const { _id, _Changed, _Created, ..._oldForm } = form.value as unknown as IMealDetails;
+        form.value = { ..._oldForm };
+    }
 
-	function clearForm() {
-		for (let key in form) {
-			if(key == 'isAvailable') form[key] = false
-			form[key as unknown as typeof IMealDetails] = "";
-		}
-	}
-
-	return { isOpen, action, request, form, menus, updateForm, clearForm, toggleModal, fetchMenus };
-})
+    return { isOpen, action, request, form, menus, updateForm, clearForm, toggleModal, fetchMenus };
+});
